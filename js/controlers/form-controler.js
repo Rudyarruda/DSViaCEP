b405ba460f32f38f52a11d3819970ff2db8ee5ce
Rudinei Arruda <rudyarruda@gmail.com>
2022-07-28 20:16:56 -0300

    Adicionadas funções para obter o json da requisição e criado evento para o botão salvar.

--- js/controlers/form-controler.js
+++ js/controlers/form-controler.js
@@ -1,5 +1,6 @@
 //FUNÇÃO CONSTRUTORA
 import Address from '../models/address.js'
+import * as requestService from '../services/request-service.js'
 
 function State() {
     
@@ -35,7 +36,17 @@ export function init() {
 
     state.btnClear.addEventListener('click',handleBtnClearClick)
     state.inputNumber.addEventListener('change', handleInputNumberChange)
+    state.btnSave.addEventListener('click', handleBtnSaveClick)
+
+
 }
+
+async function handleBtnSaveClick(event) {
+    event.preventDefault()
+    const result = await requestService.getJson('https://viacep.com.br/ws/01001000/json/')
+    console.log(result)
+}
+
 function handleBtnClearClick(event){
     event.preventDefault()
     clearForm()
